f4943c6cdf962bd33f49ec146b86888126987030
Douglas Evole <douglaseamutete@gmail.com>
2020-10-19 13:16:37 +0300

    added hover on home page

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
-// $(document).ready(function(){
-//     $("#show").mouseover(function(){
-//         $(".overlay2").toggle();
-//     })
-//     $("#show").mouseout(function(){
-//         $(".overlay2").toggle();
-//     })
+$(document).ready(function(){
+    $("#show").mouseover(function(){
+        $(".overlay2").toggle();
+    })
+    $("#show").mouseout(function(){
+        $(".overlay2").toggle();
+    })
 
-//     $("#show1").mouseover(function(){
-//     $(".overlay3").toggle()
-//    })
-// $("#show1").mouseout(function(){
-//     $(".overlay3").toggle();
-// })
-// $("#show2").mouseover(function(){
-//     $(".overlay4").toggle()
-//    })
-// $("#show2").mouseout(function(){
-//     $(".overlay4").toggle();
-// })
-// $("#show3").mouseover(function(){
-//     $(".overlay5").toggle()
-//    })
-// $("#show3").mouseout(function(){
-//     $(".overlay5").toggle();
-// })
+    $("#show1").mouseover(function(){
+    $(".overlay3").toggle()
+   })
+$("#show1").mouseout(function(){
+    $(".overlay3").toggle();
+})
+$("#show2").mouseover(function(){
+    $(".overlay4").toggle()
+   })
+$("#show2").mouseout(function(){
+    $(".overlay4").toggle();
+})
+$("#show3").mouseover(function(){
+    $(".overlay5").toggle()
+   })
+$("#show3").mouseout(function(){
+    $(".overlay5").toggle();
+})
    
-// });
+});
